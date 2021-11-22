2e3418880e510ddd10b884bd218e7f15489ce500
felipetoscanog <ftoscanogonzalez@udesa.edu.ar>
2021-11-22 19:29:02 -0300

    cambios en detail-genres

--- js/details-genres.js
+++ js/details-genres.js
@@ -7,7 +7,7 @@ console.log(qsToObject)
 let id = qsToObject.get('id');
 console.log(id);
 
-let urlGenero = ``;
+let urlGenero = `https://api.themoviedb.org/3/genre/${id}?api_key=276f9f3d3746e8cf8485bca19d3e5150&language=es`;
 
 let urlPeliculasEnGeneros = `https://api.themoviedb.org/3/discover/movie?api_key=276f9f3d3746e8cf8485bca19d3e5150&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`;
 
@@ -44,7 +44,7 @@ fetch(urlPeliculasEnGeneros)
 
         let peliculasSeleccionadas = document.querySelector('.detalleGeneroPeliculas')
         let elementosPeliculas = ''; //Se actualiza con los datos del endpoint 
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < 5; i++) {
             elementosPeliculas += `<article class="articulos">
                                     <a href="./detail-movie.html"><img src="https://image.tmdb.org/t/p/w342${data.results[i].poster_path}" alt="IMAGEN NO DISPONIBLE"></a>
                                     <h3>${data.results[i].title}</h3>
@@ -76,7 +76,7 @@ fetch(urlSeriesEnGeneros)
 
         let seriesSeleccionadas = document.querySelector('.detalleGeneroSeries')
         let elementosSeries = ''; //Se actualiza con los datos del endpoint 
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < 5; i++) {
             elementosSeries += `<article class="articulos">
                                 <a href="./detail-serie.html"><img src="https://image.tmdb.org/t/p/w342${data.results[i].poster_path}" alt="IMAGEN NO DISPONIBLE"></a>
                                 <h3>${data.results[i].original_name}</h3>
